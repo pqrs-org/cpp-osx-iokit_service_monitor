10181c4eec0fc3685e436d086d9325180604aa2e
Takayama Fumihiko <tekezo@pqrs.org>
2018-11-11 20:54:42 +0900

    version 2.0.0

--- include/pqrs/osx/iokit_service_monitor.hpp
+++ include/pqrs/osx/iokit_service_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::iokit_service_monitor v1.3.0
+// pqrs::iokit_service_monitor v2.0.0
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
