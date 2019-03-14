bce297aef78c1f4910e129193b8b215c6273aa6e
Takayama Fumihiko <tekezo@pqrs.org>
2019-03-15 01:50:26 +0900

    update vendor

--- example/vendor/cget/pkg/pqrs-org__cpp-osx-iokit_types/install/include/pqrs/osx/iokit_types/iokit_keyboard_type.hpp
+++ example/vendor/cget/pkg/pqrs-org__cpp-osx-iokit_types/install/include/pqrs/osx/iokit_types/iokit_keyboard_type.hpp
@@ -16,6 +16,15 @@ struct iokit_keyboard_type : type_safe::strong_typedef<iokit_keyboard_type, uint
   using strong_typedef::strong_typedef;
 };
 
+inline std::string make_iokit_keyboard_type_string(iokit_keyboard_type t) {
+  if (t == iokit_keyboard_type(41)) {
+    return "iso";
+  } else if (t == iokit_keyboard_type(42)) {
+    return "jis";
+  }
+  return "ansi";
+}
+
 inline std::ostream& operator<<(std::ostream& stream, const iokit_keyboard_type& value) {
   return stream << type_safe::get(value);
 }
